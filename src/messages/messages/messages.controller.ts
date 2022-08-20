c058cdfd2c0ef1b4e64f9d841554d22dfc07461a
RdotSilva <RyandotSilva@gmail.com>
2022-08-20 12:33:50 +0100

    Add templates for routes

--- src/messages/messages/messages.controller.ts
+++ src/messages/messages/messages.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
   @Get()
   listMessages() {}
+
+  @Post()
+  createMessage() {}
+
+  @Get('/:id')
+  getMessage() {}
 }
