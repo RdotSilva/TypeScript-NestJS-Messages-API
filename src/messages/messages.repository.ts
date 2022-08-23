16b2235aa29f0a16f6f0edf2f3f63f4bed44dcff
RdotSilva <RyandotSilva@gmail.com>
2022-08-23 20:54:15 +0100

    Add logic for create method

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -18,5 +18,17 @@ export class MessagesRepository {
     return messages;
   }
 
-  async create(message: string) {}
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+
+    // Generate a random ID to use for the incoming content
+    const id = Math.floor(Math.random() * 999);
+
+    // Update the messages object with the new content and ID
+    messages[id] = { id, content };
+
+    // Add the new data to the JSON file
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
 }
