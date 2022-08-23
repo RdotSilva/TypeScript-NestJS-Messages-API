6cccc78b2dc023ef8d9547d2442ccbdb6ac06838
RdotSilva <RyandotSilva@gmail.com>
2022-08-23 20:50:56 +0100

    Add logic for findAll method

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -12,7 +12,11 @@ export class MessagesRepository {
     return messages[id];
   }
 
-  async findAll() {}
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+    return messages;
+  }
 
   async create(message: string) {}
 }
