5eb5f5ee32417f4597a6f5a1d09449c3d2d844a6
RdotSilva <RyandotSilva@gmail.com>
2022-08-23 20:50:18 +0100

    Add logic for findOne method

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,7 +1,16 @@
 import { readFile, writeFile } from 'fs/promises';
 
 export class MessagesRepository {
-  async findOne(id: string) {}
+  async findOne(id: string) {
+    // Read plain text from json file
+    const contents = await readFile('messages.json', 'utf8');
+
+    // Parse plain text to object
+    const messages = JSON.parse(contents);
+
+    // Return the message by ID
+    return messages[id];
+  }
 
   async findAll() {}
 
