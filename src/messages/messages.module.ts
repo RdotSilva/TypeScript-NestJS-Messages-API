9e72d18bc7d8b2aa2ef4de788b14ddbbbd710600
RdotSilva <RyandotSilva@gmail.com>
2022-08-26 18:37:00 +0100

    Update provider list

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MessagesController } from './controllers/messages.controller';
+import { MessagesRepository } from './messages.repository';
+import { MessagesService } from './messages.service';
 
 @Module({
   controllers: [MessagesController],
+  // Providers are dependencies that we mark with @Injectable
+  providers: [MessagesService, MessagesRepository],
 })
 export class MessagesModule {}
