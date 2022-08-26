af8c34a09ea48b3b0ff8730725245e0aeac6ac83
RdotSilva <RyandotSilva@gmail.com>
2022-08-26 18:36:48 +0100

    Add Injectable properties

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,5 +1,7 @@
+import { Injectable } from '@nestjs/common';
 import { MessagesRepository } from './messages.repository';
 
+@Injectable()
 export class MessagesService {
   // Automatically assign messagesRepo as a property to this class (TypeScript shorthand)
   constructor(public messagesRepo: MessagesRepository) {}
