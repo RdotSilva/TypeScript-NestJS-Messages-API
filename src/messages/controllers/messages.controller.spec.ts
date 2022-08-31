43783598641898aee1948a5d3fd54ee9434851c1
RdotSilva <RyandotSilva@gmail.com>
2022-08-31 03:25:37 -0400

    Update setup for tests

--- src/messages/controllers/messages.controller.spec.ts
+++ src/messages/controllers/messages.controller.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MessagesController } from './messages.controller';
+import { MessagesRepository } from './../messages.repository';
+import { MessagesService } from '../messages.service';
 
 describe('MessagesController', () => {
   let controller: MessagesController;
+  let repo: MessagesRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MessagesController],
+      providers: [MessagesService, MessagesRepository],
     }).compile();
 
     controller = module.get<MessagesController>(MessagesController);
