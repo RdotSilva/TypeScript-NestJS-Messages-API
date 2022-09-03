80ff54a3fbfccbd36bae497f41e5cb93712d6269
RdotSilva <RyandotSilva@gmail.com>
2022-09-03 11:35:34 -0400

    Set up swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { MessagesModule } from './messages/messages.module';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Messages API')
+    .setDescription('API for messages')
+    .setVersion('1.0')
+    .addTag('messages')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
